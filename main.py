7bfbe50d7891abe5fe798b699d358d4bf06cf4a6
Quentin Royer <quentinroyer@pop-os.localdomain>
2018-11-12 16:43:52 +0100

    base with old application

---
+++ main.py
@@ -0,0 +1,51 @@
+import csv
+import os
+import requests
+import re
+import urllib
+from bs4 import BeautifulSoup
+
+def getDomain(myMail):
+    return re.split(r'@', myMail)[1]
+
+def pingDomain(myDomain):
+    response = os.system("ping -c 1 " + myDomain)
+    return True if response == 0 else False
+
+print(pingDomain("itescia.fr"))
+
+def testDomain(myDomain):
+    codeResult = str(urllib.urlopen(myDomain).getcode())
+    return(codeResult)
+
+
+def deleteDuplicate(myList):
+    return list(set(myList))
+
+def checkMail(myMail):
+    return re.match(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', myMail)
+
+def crawlForMail(myUrl):
+    plainPage = requests.get(myUrl).text
+    beautifulPage = str(BeautifulSoup(plainPage, "html.parser"))
+    result = re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', beautifulPage)
+    return list(set(result))
+
+
+def readCsv(myFile):
+    if os.path.isfile(myFile):
+        listResult = list()
+        with open(myFile, 'r') as myCsv:
+            spamreader = csv.reader(myCsv, delimiter=',', quotechar='|')
+            for row in spamreader:
+                listResult.append(row)
+        return listResult
+    return 'No file found'
+
+def writeCsv(myFile, myList):
+    with open(myFile, 'w+') as myCsv:
+        spamwriter = csv.writer(myCsv, delimiter=' ', quotechar='|')
+        print(myList)
+        for item in myList:
+            spamwriter.writerow([item])
+    
